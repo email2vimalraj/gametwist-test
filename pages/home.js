f58cdbeb8129915400ab71729fd60015a857301e
VimalRaj Selvam <email2vimalraj@gmail.com>
2017-05-05 20:00:01 +0530

    Added documentation to the page object

--- pages/home.js
+++ pages/home.js
@@ -1,4 +1,10 @@
 var homeCommands = {
+  /**
+   * Login with the given username and password
+   * @param username The username of the user
+   * @param password The password of the user
+   * @returns a context
+   */
   login: function(username, password) {
     this.waitForElementVisible("@body", 1000)
       .assert.title("GameTwist Casino, Games & more | Play for Free")
@@ -12,6 +18,10 @@ var homeCommands = {
       .click("@loginButton");
   },
 
+  /**
+   * Closes the popup if appears
+   * @returns the context
+   */
   closePopUp: function() {
     return this.api.element("css selector", "@closeButtonInPopup", function(result) {
       if (result.status !== -1) {
@@ -22,33 +32,63 @@ var homeCommands = {
     });
   },
 
+  /**
+   * Checks whether the given username is logged in
+   * @param username The username to verify
+   * @returns the context
+   */
   isLoggedIn: function(username) {
     return this.waitForElementVisible("@nickname", 3000)
       .expect.element("@nickname").text.to.equal(username);
   },
 
+  /**
+   * Navigates to the Slots page
+   * @returns the context
+   */
   navigateToSlotsPage: function() {
     return navigateToPage(this, "@slotsButton", "Slots");
   },
 
+  /**
+   * Navigates to the Bingo page
+   * @returns the context
+   */
   navigateToBingoPage: function () {
     return navigateToPage(this, "@bingoButton", "Bingo");
   },
 
+  /**
+   * Navigates to the Casino page
+   * @returns the context
+   */
   navigateToCasinoPage: function () {
     return navigateToPage(this, "@casinoButton", "Casino");
   },
 
+  /**
+   * Navigates to the Poker page
+   * @returns the context
+   */
   navigateToPokerPage: function () {
     return navigateToPage(this, "@pokerButton", "Poker");
   },
 
+  /**
+   * Searches the given game keyword
+   * @param searchKeyword The search keyword
+   */
   searchGame: function(searchKeyword) {
     this.setValue("@searchInput", searchKeyword);
     this.api.pause(1000);
     this.waitForElementVisible("@searchItem", 3000);
   },
 
+  /**
+   * Selects the item in the search results
+   * @param callback The callback which yields the selected item
+   * @returns the context
+   */
   selectItem: function(callback) {
     this.api.elements("css selector", this.elements.searchItem.selector, function(result) {
       console.log("The number of shown games are: " + result.value.length);
@@ -65,11 +105,20 @@ var homeCommands = {
     return this;
   },
 
+  /**
+   * Verifies the current page name with the given pagename
+   * @param pagename The pagename to be verified
+   * @returns this context
+   */
   verifyPageName: function(pagename) {
     return this.waitForElementVisible("@breadCrumb", 3000)
       .expect.element("@breadCrumb").text.to.contains(pagename);
   },
 
+  /**
+   * Selects the language as German
+   * @returns this context
+   */
   selectGermanLanguage: function () {
     this.moveToElement("@selectLanguageButton", 10, 10)
       .waitForElementVisible("@deutschLanguageButton", 3000)
@@ -78,6 +127,10 @@ var homeCommands = {
     return this;
   },
 
+  /**
+   * Selects the language as English
+   * @returns this context
+   */
   selectEnglishLanguage: function () {
     this.moveToElement("@selectLanguageButton", 10, 10)
       .waitForElementVisible("@englishLanguageButton", 3000)
@@ -86,25 +139,48 @@ var homeCommands = {
     return this;
   },
 
+  /**
+   * Checks the language is changed to german
+   * @returns this context
+   */
   isLanguageChangedToGerman: function () {
     return this.expect.element("@buyTwistsButton").text.to.equal("TWISTS KAUFEN");
   },
 
+  /**
+   * Checks the language is changed to english
+   * @returns this context
+   */
   isLanguageChangedToEnglish: function () {
     return this.expect.element("@buyTwistsButton").text.to.equal("BUY TWISTS");
   },
 
+  /**
+   * Logouts the user
+   * @returns this context
+   */
   logoutUser: function() {
     return this.moveToElement("@userMenuLink", 10, 10)
       .waitForElementVisible("@logoutButton", 3000)
       .click("@logoutButton");
   },
 
+  /**
+   * Checks whether the user is logged out
+   * @returns this context
+   */
   isUserLoggedOut: function () {
     return this.expect.element("@username").to.be.visible.before(5000);
   }
 };
 
+/**
+ * A helper function used for navigation
+ * @param driver The driver context
+ * @param urlElement The url element
+ * @param pageName The name of the page
+ * @returns {boolean} True if success; false, otherwise.
+ */
 function navigateToPage(driver, urlElement, pageName) {
   return driver.click(urlElement)
     .waitForElementVisible("@highlightedButton", 3000)
